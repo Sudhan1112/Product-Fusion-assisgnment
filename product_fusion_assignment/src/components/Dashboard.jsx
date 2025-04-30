8e84747a4b24816ce4788ed9df7d5e82043abad3
sudhan1112 <sudhanssudhan83@gmail.com>
2025-04-30 19:48:34 +0530

    Deployment process

--- product_fusion_assignment/src/components/Dashboard.jsx
+++ product_fusion_assignment/src/components/Dashboard.jsx
@@ -204,9 +204,9 @@ const Dashboard = () => {
 
   // Helper function to format currency
   const formatCurrency = (value) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD'
+      currency: 'INR'
     }).format(value);
   };
 
